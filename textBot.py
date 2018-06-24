d50fefd18ad2d716922a4e37ef8921031ef7253d
DanPetruso <31451444+DanPetruso@users.noreply.github.com>
2018-06-24 12:49:32 -0400

    bug fixes
    fixing up and testing the methods, starting to add a main method that the user can use

--- textBot.py
+++ textBot.py
@@ -1,6 +1,5 @@
 from twilio.rest import Client
 import sqlite3
-import quotesManager
 
 conn = sqlite3.connect("contacts.db")
 c = conn.cursor()
@@ -15,7 +14,7 @@ def easyCommand(command):
     return command.strip().lower()
 
 def createContactTable():
-    c.execute("""CREATE TABLE contacts (
+    c.execute("""CREATE TABLE IF NOT EXISTS contacts (
                 first text,
                 last text,
                 number text
@@ -23,35 +22,26 @@ def createContactTable():
     
     conn.commit()
 
-
 def addContact():
+    print("Adding Contact")
     firstName = input("First Name:")
     lastName = input("Last Name:")
     firstName.strip()
     lastName.strip()
-    num = input("Number for",firstName,lastName,"(no (), - or spaces)")
+    num = input("Number for " + firstName +" "+ lastName + "(no (), - or spaces): ")
     
-    yesNo = easyCommand(input("Is",num,"the correct number for",firstName,lastName + "? (y/n)"))
+    yesNo = easyCommand(input("Is " + num + " the correct number for " + firstName +" "+ lastName + "? (y/n)"))
     if yesNo == "y" or yesNo == "yes":
         c.execute("INSERT INTO contacts VALUES (?,?,?)",(firstName,lastName,num))
         conn.commit()
     
     
 def getContact(firstName,lastName):
-    #user only enters first name
-    if lastName == "":
-        c.execute("SELECT * FROM contacts WHERE first=?",(firstName))
-    else:
-        c.execute("SELECT * FROM contacts WHERE first=? AND last=?",(firstName,lastName))
+    c.execute("SELECT * FROM contacts WHERE first=? AND last=?",(firstName,lastName))
     people = c.fetchall()
-    
-    #contact not found, checks if user entered lastName where firstName should have been
-    if people == None:
-        c.execute("SELECT * FROM contacts WHERE first=?",(lastName))
-        people = c.fetchall()
         
-    #contact still not found, asks if user wants to add it
-    if people == None:
+    #contact not found
+    if len(people) == 0:
         yesNo = easyCommand(input("Contact not found. Would you like to add one?(y/n)\n"))
         
         if yesNo == "y" or yesNo == "yes":
@@ -64,13 +54,14 @@ def getContact(firstName,lastName):
         else:
             yesNo = easyCommand(input("Would you like to add a new contact?"))
             addContact()
-            
+    
+    #if multiple contacts witht the same name are found
     else:
         print(len(people),"contacts found. Please select the number corresponding to the contact you want.")
         for i in range(0,len(people)):
-            print(i," | ",people[i][0],"\t",people[i][0],"\t",people[i][2])
-        print(i+1," | ", "contact not found")
-        choice = input()
+            print(i," | ",people[i][0],"\t",people[i][1],"\t",people[i][2])
+        print(len(people)," | ", "contact not found")
+        choice = int(input())
         if choice >= 0 and choice < i:
             sendMessage(people[choice][2])
         elif choice == i:
@@ -79,7 +70,7 @@ def getContact(firstName,lastName):
                 addContact()
             
 def sendMessage(num):
-
+    print("text")
     #message = client.messages.create(
     #    to=, 
     #    from_=tnum,
@@ -96,13 +87,20 @@ class Quote:
 
 
 if __name__ == '__main__':
-    while True:
-        print("What would you like to do?")
-        print("0 | Send Message \n"
-              "1 | Add Contact  \n"
-              "2 | See Contact List  \n"
-              )
-        choice = input()
+    createContactTable()
+    
+    print("What would you like to do?")
+    print("0 | Get Contact To Send a Message To \n"
+          "1 | Add Contact  \n"
+          "2 | See Contact List  \n"
+          )
+    choice = int(input())
+    if choice == 0:
+        first = input("Enter the first name of the contact. ")
+        last = input("Enter the last name of the contact. ")
+        getContact(first,last)
+    if choice == 1:
+        addContact()
     
     
 
